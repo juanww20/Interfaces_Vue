7d1957269f8c545e9f13a12b343d141c9ef982fc
Juan Wu <juanwu8238@gmail.com>
2025-07-08 00:03:50 -0400

    Agrega SectionService y ContactoSection

--- Interfaces_vue/src/views/HomePage.vue
+++ Interfaces_vue/src/views/HomePage.vue
@@ -1,9 +1,13 @@
 <template>
     <ComponenteBanner></ComponenteBanner>
     <ComponenteAboutSection></ComponenteAboutSection>
+    <ComponenteSectionService></ComponenteSectionService>
+    <ComponenteContactoSection></ComponenteContactoSection>
 </template>
 
 <script setup>
 import ComponenteBanner from '@/components/ComponenteBanner.vue';
 import ComponenteAboutSection from '@/components/ComponenteAboutSection.vue';
+import ComponenteSectionService from '@/components/ComponenteSectionService.vue';
+import ComponenteContactoSection from '@/components/ComponenteContactoSection.vue';
 </script>
