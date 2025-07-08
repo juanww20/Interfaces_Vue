ce6deb385cf30a8f9307d6a409f9fb9c8bb0485b
Juan Wu <juanwu8238@gmail.com>
2025-07-07 23:53:52 -0400

    Agrega Componente Footer

--- Interfaces_vue/src/App.vue
+++ Interfaces_vue/src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <componente-header></componente-header>
   <router-view></router-view>
+  <componente-footer></componente-footer>
 </template>
 
 <script setup>
 import {RouterView } from 'vue-router'
 import ComponenteHeader from './components/ComponenteHeader.vue';
+import ComponenteFooter from './components/ComponenteFooter.vue';
 </script>
 
 <style scoped>
