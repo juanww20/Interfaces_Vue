4b771ac4b6a8c6c06a12e809109a699f54c4c2bf
Juan Wu <juanwu8238@gmail.com>
2025-07-08 00:00:16 -0400

    Agrega Componente AboutSection

--- Interfaces_vue/src/views/HomePage.vue
+++ Interfaces_vue/src/views/HomePage.vue
@@ -1,7 +1,9 @@
 <template>
     <ComponenteBanner></ComponenteBanner>
+    <ComponenteAboutSection></ComponenteAboutSection>
 </template>
 
 <script setup>
 import ComponenteBanner from '@/components/ComponenteBanner.vue';
+import ComponenteAboutSection from '@/components/ComponenteAboutSection.vue';
 </script>
