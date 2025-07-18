bf054846b1cc08d74821834d08492dea907372a6
Sperrotta10 <santiagoperrotta10@gmail.com>
2025-07-18 15:40:36 -0400

    feat: lo mismo que el comiit anterior

--- Interfaces_vue/src/App.vue
+++ Interfaces_vue/src/App.vue
@@ -9,12 +9,16 @@ import {RouterView } from 'vue-router'
 import ComponenteHeader from './components/ComponenteHeader.vue';
 import ComponenteFooter from './components/ComponenteFooter.vue';
 import { useAuthStore } from '@/stores/Auth';
+import { useStyleStore } from '@/stores/Styles';
 import { onMounted } from 'vue';
 
 const auth = useAuthStore()
+const styles = useStyleStore()
 
 onMounted(async () => {
   await auth.checkSession()
+  styles.loadPaletteFromStorage()
+  styles.loadFontFromStorage()
 })
 
 
