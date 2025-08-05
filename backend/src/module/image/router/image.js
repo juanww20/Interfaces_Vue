12477423dd050be9f477efbeaf2337d36347e924
Sperrotta10 <santiagoperrotta10@gmail.com>
2025-08-05 16:16:58 -0400

    feat (router module image): Se implemento un middleware para manejar la subida de la imagen, por lo cual ayuda a mejorar la estructura y la organizacion del codigo

--- backend/src/module/image/router/image.js
+++ backend/src/module/image/router/image.js
@@ -1,36 +1,11 @@
 import { Router } from "express";
 import { ControllerImage } from "../controller/image.js";
-import multer from "multer";
-import fs from 'fs';
-import path from 'path';
-import { UPLOADS_DIR } from "../../../utils/global-path.js";
-
-const __dirname = UPLOADS_DIR;
+import { uploadImage } from "../../../middlewares/image/upload_image.js";
 
 export const routerImage = Router();
 
-// 1. Configuración de multer
-
-const imageUploadPath = path.join(__dirname, 'uploads', 'images');
-
-if (!fs.existsSync(imageUploadPath)) {
-  fs.mkdirSync(imageUploadPath, { recursive: true });
-}
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, imageUploadPath);
-  },
-  filename: function (req, file, cb) {
-    const uniqueName = Date.now() + '-' + file.originalname;
-    cb(null, uniqueName);
-  }
-});
-const upload = multer({ storage });
-
-
-routerImage.post("/", upload.single("image"), ControllerImage.createImage);
+routerImage.post("/", uploadImage.single("image"), ControllerImage.createImage);
 routerImage.get("/", ControllerImage.getAllImages);
 routerImage.get("/:id", ControllerImage.getImageById);
-routerImage.put("/:id", upload.single("image"), ControllerImage.updateImage);
+routerImage.put("/:id", uploadImage.single("image"), ControllerImage.updateImage);
 routerImage.delete("/:id", ControllerImage.deleteImage);
