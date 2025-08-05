3a6ba2f9cc05a389ee6772198c6dad943071e38f
Sperrotta10 <santiagoperrotta10@gmail.com>
2025-08-05 14:46:56 -0400

    feat (models tables.js): Seimplento la definicion de la tabla para manejar el modulo de imagenes

--- backend/src/models/tables.js
+++ backend/src/models/tables.js
@@ -6,47 +6,28 @@ import { defineFont } from "../module/font/schemas/font.js";
 import { defineFontFamily } from "../module/font/schemas/font_family.js";
 import { defineUser } from "../module/auth/schemas/register.js"
 import { defineRole } from "../module/auth/schemas/role.js";
+import { defineImage } from "../module/image/schema/imagen.js";
 
 const Palette = definePalette(sequelize, DataTypes);
 const Colors = defineColors(sequelize, DataTypes);
 const Font = defineFont(sequelize, DataTypes);
 const FontFamily = defineFontFamily(sequelize, DataTypes);
+const Image = defineImage(sequelize, DataTypes);
 const User = defineUser(sequelize, DataTypes);
 const Role = defineRole(sequelize, DataTypes);
 
 // Relaciones entre tablas
 
-Palette.belongsTo(Colors, {
-    foreignKey : 'colors_id',
-    as : 'colors',
-    onDelete: 'CASCADE'
-})
+Palette.belongsTo(Colors, { foreignKey : 'colors_id', as : 'colors', onDelete: 'CASCADE' })
 
-Colors.hasOne(Palette, {
-    foreignKey : 'colors_id',
-    as : 'palette',
-})
+Colors.hasOne(Palette, { foreignKey : 'colors_id', as : 'palette' })
 
-Font.belongsTo(FontFamily, {
-    foreignKey : 'fontFamily_id',
-    as : 'fontFamily',
-    onDelete: 'CASCADE'
-})
+Font.belongsTo(FontFamily, { foreignKey : 'fontFamily_id',as : 'fontFamily',onDelete: 'CASCADE' })
 
-FontFamily.hasOne(Font, {
-    foreignKey : 'fontFamily_id',
-    as : 'font',
-    onDelete: 'CASCADE'
-})
+FontFamily.hasOne(Font, { foreignKey : 'fontFamily_id', as : 'font',onDelete: 'CASCADE' })
 
-User.belongsTo(Role, {
-    foreignKey : 'role_id',
-    as : 'role',
-})
+User.belongsTo(Role, { foreignKey : 'role_id', as : 'role' })
 
-Role.hasMany(User, {
-    foreignKey : 'role_id',
-    as : 'user',
-})
+Role.hasMany(User, { foreignKey : 'role_id', as : 'user' })
 
-export { Palette, Colors, Font, FontFamily, User, Role };
+export { Palette, Colors, Font, FontFamily, User, Role, Image };
