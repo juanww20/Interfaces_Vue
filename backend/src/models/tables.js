a5e4f72d0e5b9ee8e7d6ffa23f35fe5710ac70c3
Sperrotta10 <santiagoperrotta10@gmail.com>
2025-08-06 15:12:59 -0400

    feat (models tables.js): Se implemento las tablas de video, audio, subtitulos y sus relaciones

--- backend/src/models/tables.js
+++ backend/src/models/tables.js
@@ -7,12 +7,18 @@ import { defineFontFamily } from "../module/font/schemas/font_family.js";
 import { defineUser } from "../module/auth/schemas/register.js"
 import { defineRole } from "../module/auth/schemas/role.js";
 import { defineImage } from "../module/image/schema/imagen.js";
+import { defineVideo } from "../module/video/schema/video.js";
+import { defineAudio } from "../module/audio/schema/audio.js";
+import { defineSubtitulos } from "../module/subtitulos_video/schema/subtitulo.js";
 
 const Palette = definePalette(sequelize, DataTypes);
 const Colors = defineColors(sequelize, DataTypes);
 const Font = defineFont(sequelize, DataTypes);
 const FontFamily = defineFontFamily(sequelize, DataTypes);
 const Image = defineImage(sequelize, DataTypes);
+const Video = defineVideo(sequelize, DataTypes);
+const Audio = defineAudio(sequelize, DataTypes);
+const Subtitulo = defineSubtitulos(sequelize, DataTypes);
 const User = defineUser(sequelize, DataTypes);
 const Role = defineRole(sequelize, DataTypes);
 
@@ -30,4 +36,10 @@ User.belongsTo(Role, { foreignKey : 'role_id', as : 'role' })
 
 Role.hasMany(User, { foreignKey : 'role_id', as : 'user' })
 
-export { Palette, Colors, Font, FontFamily, User, Role, Image };
+Video.hasMany(Audio, { foreignKey: 'video_id', as: 'audios' });
+Audio.belongsTo(Video, { foreignKey: 'video_id', as: 'video', onDelete: 'CASCADE' });
+
+Video.hasMany(Subtitulo, { foreignKey: 'video_id', as: 'subtitulos' });
+Subtitulo.belongsTo(Video, { foreignKey: 'video_id', as: 'video', onDelete: 'CASCADE' });
+
+export { Palette, Colors, Font, FontFamily, User, Role, Image, Video, Audio, Subtitulo };
