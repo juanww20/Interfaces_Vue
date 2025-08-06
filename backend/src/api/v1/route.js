716ef606da9e4260c08b09d10ce6d09024a9c642
Sperrotta10 <santiagoperrotta10@gmail.com>
2025-08-06 19:51:45 -0400

    feat (app.js and api/v1): Se implemento el endpoint del modulo de subtitulo y se establecio en apps.js el folder de subtitulos para que se accesible de manera publica

--- backend/src/api/v1/route.js
+++ backend/src/api/v1/route.js
@@ -4,6 +4,7 @@ import { routerAuth } from "../../module/auth/router/auth.js";
 import { routerImage } from "../../module/image/router/image.js";
 import { routerVideo } from "../../module/video/router/video.js";
 import { routerAudio } from "../../module/audio/router/audio.js";
+import { routerSubtitulo } from "../../module/subtitulos_video/router/subtitulo.js";
 import { Router } from "express";
 
 export const routerApi = Router();
@@ -14,3 +15,4 @@ routerApi.use('/auth', routerAuth);
 routerApi.use('/image', routerImage);
 routerApi.use('/video', routerVideo);
 routerApi.use('/audio', routerAudio);
+routerApi.use('/subtitle', routerSubtitulo);
